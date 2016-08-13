be828e1ee17037f0b9244cd2490f5b487d501891
pepinpin <pepinpin@users.noreply.github.com>
2016-08-13 14:09:58 +0200

    Use of Apache IO library
    - changed the methodology of getting the json file from the assets folder by using the Apache IO library

--- GetJson.java
+++ GetJson.java
@@ -1,71 +1,70 @@
 import android.app.Activity;
+import android.widget.Toast;
+
+import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-public class GetJSON {
+class GetJSON {
 
 	private JSONObject _resultJSONobject;
 	private Map<String, String> _resultHashMap = new HashMap<>();
+
+	private int tryCounter = 0;
+	private static final int TRY_MAX_COUNT = 10;
 
 
 	private void getObjectFromAssets(Activity activity, String fileName){
 
 		try {
 			InputStream is = activity.getAssets().open(fileName);
-			int size = is.available();
-			byte[] buffer = new byte[size];
-			is.read(buffer);
-			is.close();
-			String json = new String(buffer, "UTF-8");
-			
+			String json = IOUtils.toString(is);
 			_resultJSONobject = new JSONObject(json);
-			
+
 		} catch (IOException | JSONException ex) {
 			ex.printStackTrace();
 		}
 	}
 
-	public Map<String, String> getHashMapFromAssets(Activity activity, String fileName) {
+	Map<String, String> getHashMapFromAssets(Activity activity, String fileName) {
 
-		getObjectFromAssets(activity, fileName);
+		// to handle the possibility of some problems accessing the JSON file
+		// we try x times to get the file and display a toast if it fails
 
-		Iterator<String> keys = _resultJSONobject.keys();
-		while(keys.hasNext()){
+		while(tryCounter <= TRY_MAX_COUNT
+				&& _resultJSONobject == null){
 
-			final String currentKey = keys.next();
-			String currentValue = "";
-
-			try {
-				currentValue = (String) _resultJSONobject.get(currentKey);
-			} catch (JSONException e1) {
-				e1.printStackTrace();
-			}
-			_resultHashMap.put(currentKey, currentValue);
+			tryCounter++;
+			getObjectFromAssets(activity, fileName);
 		}
 
+		if (_resultJSONobject != null){
+
+			Iterator<String> keys = _resultJSONobject.keys();
+			while(keys.hasNext()){
+
+				final String currentKey = keys.next();
+				String currentValue = "";
+
+				try {
+					currentValue = (String) _resultJSONobject.get(currentKey);
+				} catch (JSONException e1) {
+					e1.printStackTrace();
+				}
+				_resultHashMap.put(currentKey, currentValue);
+			}
+
+		}else{
+			Toast.makeText(activity, R.string.error_no_json_toast, Toast.LENGTH_SHORT).show();
+		}
+
+		tryCounter = 0;
 		return _resultHashMap;
-	}
-
-	public JSONObject getJSONobjectFromAssets(Activity activity, String fileName) {
-
-		getObjectFromAssets(activity, fileName);
-		return _resultJSONobject;
-	}
-
-	public void getHashMapFromHttp() throws RuntimeException{
-
-		// todo : finish this method
-		throw new RuntimeException();
-	}
-
-	public void getJSONobjectFromHttp() throws RuntimeException{
-
-		// todo : finish this method
-		throw new RuntimeException();
 	}
 }
